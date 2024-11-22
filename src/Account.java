75c917d9ad74435c061d3afd0de6dbd72a447113
eilidh boyd <eilidh.boyd.2022@uni.strath.ac.uk>
2024-11-22 15:30:01 +0000

    small consistency fixes

--- src/Account.java
+++ src/Account.java
@@ -9,82 +9,82 @@ public class Account {
     private String password;
 
     // different balances for each exchange currency
-    private float gbp_balance;
-    private float usd_balance;
-    private float euro_balance;
-    private float yen_balance;
+    private float gbpBalance;
+    private float usdBalance;
+    private float euroBalance;
+    private float yenBalance;
 
     public Account(String username, String password) {
         this.username = username;
         this.password = password;
-        this.gbp_balance = 0;
-        this.usd_balance = 0;
-        this.euro_balance = 0;
-        this.yen_balance = 0;
+        this.gbpBalance = 0;
+        this.usdBalance = 0;
+        this.euroBalance = 0;
+        this.yenBalance = 0;
     }
 
-    public float get_gbp_balance() {
-        return this.gbp_balance;
+    public float getGbpBalance() {
+        return this.gbpBalance;
     }
 
-    public float get_usd_balance() {
-        return this.usd_balance;
+    public float getUsdBalance() {
+        return this.usdBalance;
     }
 
-    public float get_euro_balance() {
-        return this.euro_balance;
+    public float getEuroBalance() {
+        return this.euroBalance;
     }
 
-    public float get_yen_balance() {
-        return this.yen_balance;
+    public float getYenBalance() {
+        return this.yenBalance;
     }
 
-    public void set_gbp_balance(float gbp_balance) {
-        this.gbp_balance = gbp_balance;
+    public void setGbpBalance(float gbp_balance) {
+        this.gbpBalance = gbp_balance;
     }
 
-    public void set_usd_balance(float usd_balance) {
-        this.usd_balance = usd_balance;
+    public void setUsdBalance(float usd_balance) {
+        this.usdBalance = usd_balance;
     }
 
-    public void set_euro_balance(float euro_balance) {
-        this.euro_balance = euro_balance;
+    public void setEuroBalance(float euro_balance) {
+        this.euroBalance = euro_balance;
     }
 
-    public void set_yen_balance(float yen_balance) {
-        this.yen_balance = yen_balance;
+    public void setYenBalance(float yen_balance) {
+        this.yenBalance = yen_balance;
     }
 
     public void addToGbpBalance(float amount){
-        this.gbp_balance += amount;
+        this.gbpBalance += amount;
     }
 
     public void addToEuroBalance(float amount){
-        this.euro_balance += amount;
+        this.euroBalance += amount;
     }
 
     public void addToUsdBalance(float amount){
-        this.usd_balance += amount;
+        this.usdBalance += amount;
     }
 
     public void addToYenBalance(float amount){
-        this.yen_balance += amount;
+        this.yenBalance += amount;
     }
 
     public void decreaseGbpBalance(float amount){
-        this.gbp_balance -= amount;
+        this.gbpBalance -= amount;
     }
 
     public void decreaseEuroBalance(float amount){
-        this.euro_balance -= amount;
+        this.euroBalance -= amount;
     }
 
     public void decreaseUsdBalance(float amount){
-        this.usd_balance -= amount;
+        this.usdBalance -= amount;
     }
 
     public void decreaseYenBalance(float amount){
-        this.yen_balance -= amount;
+        this.yenBalance -= amount;
     }
 
     // This method needs to use the accounts stored on server - using a regular file right now for testing purposes
@@ -113,7 +113,11 @@ public class Account {
     public void createAccount(String username, String password) throws IOException {
         File accounts = new File("src/bankAccounts.txt");
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(accounts, true))){
-            writer.write(username + "," + password + "," + 0 + "," + 0 + "," + 0 + "," + 0);
+            setGbpBalance(0.0F);
+            setEuroBalance(0.0F);
+            setUsdBalance(0.0F);
+            setYenBalance(0.0F);
+            writer.write(username + "," + password + "," + getGbpBalance() + "," + getUsdBalance() + "," + getEuroBalance() + "," + getYenBalance());
             writer.newLine();
         }
         catch (IOException e){
